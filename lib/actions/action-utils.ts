97a053e05504fa14c228ed52be01173b165f9efa
Jack Agtual <jaagtual@gmail.com>
2026-02-11 22:07:32 -0800

    Helper function for action validation

--- lib/actions/action-utils.ts
+++ lib/actions/action-utils.ts
@@ -1,9 +1,17 @@
 import { FieldPath, FieldValues } from "react-hook-form";
 import { z } from "zod";
 import { $ZodIssue } from "zod/v4/core";
+import {
+  ActionInputs,
+  FormError,
+  InputValidationResponse,
+  ValidatedShape,
+} from "../types/action-types";
 import { mongoIdSchema } from "../types/mongo-types";
 
-export function getErrors<T extends FieldValues>(issues: $ZodIssue[]) {
+export function getErrors<T extends FieldValues>(
+  issues: $ZodIssue[]
+): FormError<T>[] {
   return issues.map((issue) => {
     return {
       path: issue.path.join(".") as FieldPath<T>,
@@ -12,6 +20,7 @@ export function getErrors<T extends FieldValues>(issues: $ZodIssue[]) {
   });
 }
 
+// TODO: Delete
 export function validateId(id: unknown) {
   const idParseResult = mongoIdSchema.safeParse(id);
   if (!idParseResult.success) {
@@ -20,10 +29,62 @@ export function validateId(id: unknown) {
   return idParseResult;
 }
 
+// TODO: Delete
 export function validatePath(path: unknown) {
   const pathParseResult = z.string().safeParse(path);
   if (!pathParseResult.success) {
     throw new Error("Invalid path");
   }
   return pathParseResult;
+}
+
+function safeParseId(id: unknown) {
+  return mongoIdSchema.safeParse(id);
+}
+
+function safeParsePath(path: unknown) {
+  return z.string().safeParse(path);
+}
+
+export function validateActionInputs<
+  T extends FieldValues,
+  U extends ActionInputs = ActionInputs
+>({ entryId, statementId, path }: U): InputValidationResponse<T, U> {
+  const data = {} as ValidatedShape<U>;
+  const errors: FormError<T>[] = [];
+
+  if (entryId) {
+    const parseResult = safeParseId(entryId);
+    if (parseResult.success) {
+      data.entryId = parseResult.data;
+    } else {
+      errors.push({ path: "root", message: "Invalid entryId" });
+    }
+  }
+
+  if (statementId) {
+    const parseResult = safeParseId(statementId);
+    if (parseResult.success) {
+      data.statementId = parseResult.data;
+    } else {
+      errors.push({ path: "root", message: "Invalid statementId" });
+    }
+  }
+
+  if (path) {
+    const parseResult = safeParsePath(path);
+    if (parseResult.success) {
+      data.path = parseResult.data;
+    } else {
+      errors.push({ path: "root", message: "Invalid path" });
+    }
+  }
+
+  if (errors.length) {
+    return {
+      success: false,
+      errors,
+    };
+  }
+  return { success: true, data };
 }
--- lib/types/action-types.ts
+++ lib/types/action-types.ts
@@ -5,9 +5,39 @@ export type FormError<T extends FieldValues> = {
   message: string;
 };
 
+type FailedActionResponse<T extends FieldValues> = {
+  success: false;
+  errors: FormError<T>[];
+};
+
 export type ActionResponse<T extends FieldValues> =
   | { success: true }
+  | FailedActionResponse<T>;
+
+export type ActionInputs = {
+  entryId?: unknown;
+  statementId?: unknown;
+  path?: unknown;
+};
+
+type ValidatedActionInputs = {
+  entryId: string;
+  statementId: string;
+  path: string;
+};
+
+export type ValidatedShape<T> = {
+  [K in keyof T]-?: K extends keyof ValidatedActionInputs
+    ? ValidatedActionInputs[K]
+    : never;
+};
+
+export type InputValidationResponse<
+  T extends FieldValues,
+  U extends ActionInputs
+> =
   | {
-      success: false;
-      errors: FormError<T>[];
-    };
+      success: true;
+      data: ValidatedShape<U>;
+    }
+  | FailedActionResponse<T>;
