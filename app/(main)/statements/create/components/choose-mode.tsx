dca4c5f405d40a1d70a8b3205c77e835038be61c
Jack Agtual <jaagtual@gmail.com>
2026-03-08 00:14:07 -0800

    Autoamatically selecting blank form if user doesn't have previous statement

--- app/(main)/statements/create/components/choose-mode.tsx
+++ app/(main)/statements/create/components/choose-mode.tsx
@@ -12,24 +12,31 @@ import dbConnect from "@/lib/db/mongodb";
 import { ExternalLinkIcon } from "lucide-react";
 import Link from "next/link";
 import PrefillForm from "./prefill-form";
+import { redirect } from "next/navigation";
 
 export default async function ChooseMode() {
   await dbConnect();
   const session = await checkSession();
 
   const blankParams = new URLSearchParams({ mode: "blank" }).toString();
+  const blankFormHref = `/statements/create?${blankParams}`;
 
   const statements = await Statement.find({ userId: session.user.id }).sort({
     year: -1,
   }); // select year not working
   const years = statements.map((statement) => statement.year);
 
+  // User doesn't have any statements
+  if (years.length === 0) {
+    redirect(blankFormHref);
+  }
+
   return (
     <>
       <div>Choose mode</div>
       <ItemGroup className="gap-y-2">
         <Item asChild variant="outline">
-          <Link href={`/statements/create?${blankParams}`}>
+          <Link href={blankFormHref}>
             <ItemContent>
               <ItemTitle>Blank</ItemTitle>
               <ItemDescription>Start with an empty form</ItemDescription>
